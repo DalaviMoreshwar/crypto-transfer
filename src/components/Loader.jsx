7f5221b4f7940811319690b2c9b429e404273ffe
Moreshwar Dalvi <moreshwar@anew-da.com>
2022-07-01 14:00:47 +0530

    added welcome and loader section, tailwind config configurations

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-5s00" />
+    </div>
+  );
 };
 
 export default Loader;
